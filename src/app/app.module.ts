bbb8f3b5aa8fe43d121bc5c71b0790d962ba0c9f
horndron <horndron@yandex.ru>
2022-04-29 21:59:58 +0300

    refactor: add app-store module and delete redux folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { EffectsModule } from '@ngrx/effects';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { reducers, metaReducers } from './redux/reducers';
 import { environment } from '../environments/environment';
-import { AppEffects } from './redux/effects/app.effects';
+import { AppRoutingModule } from './app-routing.module';
+import { AppStoreModule } from './app-store.module';
+import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
@@ -19,11 +15,8 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    StoreModule.forRoot(reducers, {
-      metaReducers,
-    }),
+    AppStoreModule,
     environment.production ? [] : StoreDevtoolsModule.instrument({ maxAge: 25 }),
-    EffectsModule.forRoot([AppEffects]),
   ],
   providers: [],
   bootstrap: [AppComponent],
