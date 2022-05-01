5bf7775c5cac4886e914a95ff9106ee6b5eb623a
horndron <horndron@yandex.ru>
2022-05-01 11:54:29 +0300

    feat: add routing for main-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./main-page/main-page.module').then((m) => m.MainPageModule),
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
