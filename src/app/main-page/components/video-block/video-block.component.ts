d7b736515c2e23a1100f78d06d38b198986d761c
horndron <horndron@yandex.ru>
2022-05-04 10:44:46 +0300

    feat: add full style for >1200px and add animations

--- src/app/main-page/components/video-block/video-block.component.ts
+++ src/app/main-page/components/video-block/video-block.component.ts
@@ -1,12 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'rsm-video-block',
   templateUrl: './video-block.component.html',
   styleUrls: ['./video-block.component.scss'],
 })
-export class VideoBlockComponent {
+export class VideoBlockComponent implements OnInit {
   @Input() title!: string;
 
   @Input() videoId!: string;
+
+  video = false;
+
+  videoUrl!: SafeResourceUrl;
+
+  constructor(private domsanitazer: DomSanitizer) {}
+
+  ngOnInit() {
+    this.getVideoUrl();
+  }
+
+  getVideoUrl(): void {
+    this.videoUrl = this.domsanitazer
+      .bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${this.videoId}?autoplay=1`);
+  }
+
+  onVideo(): void {
+    this.video = true;
+  }
 }
--- src/app/main-page/pages/main-page/main-page.component.ts
+++ src/app/main-page/pages/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { PrimeNGConfig } from 'primeng/api';
 import { environment } from '../../../../environments/environment.prod';
@@ -8,7 +8,7 @@ import { environment } from '../../../../environments/environment.prod';
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss'],
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent implements OnInit, AfterViewInit {
   constructor(
     private primengConfig: PrimeNGConfig,
     private translateService: TranslateService,
@@ -17,5 +17,23 @@ export class MainPageComponent implements OnInit {
   ngOnInit() {
     this.primengConfig.ripple = true;
     this.translateService.use(environment.defaultLocale);
+  }
+
+  ngAfterViewInit() {
+    this.addAnimation();
+  }
+
+  addAnimation() {
+    function onEntry(entry: IntersectionObserverEntry[]) {
+      entry.forEach((change) => {
+        if (change.isIntersecting) {
+          change.target.classList.add('element-show');
+        }
+      });
+    }
+    const options = { threshold: [0.5] };
+    const observer: IntersectionObserver = new IntersectionObserver(onEntry, options);
+    const elements = document.querySelectorAll('.element-animation');
+    elements.forEach((elm) => observer.observe(elm));
   }
 }
