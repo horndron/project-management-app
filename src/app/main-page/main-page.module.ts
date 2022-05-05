e593103aa85b0cb305214653e80381c025397323
horndron <horndron@yandex.ru>
2022-05-05 17:34:40 +0300

    refactor: delete unnecessary component

--- src/app/main-page/main-page.module.ts
+++ src/app/main-page/main-page.module.ts
@@ -6,7 +6,6 @@ import { TranslateModule } from '@ngx-translate/core';
 import { MainPageRoutingModule } from './main-page-routing.module';
 import { CardWithIconComponent } from './components/card-with-icon/card-with-icon.component';
 import { VideoBlockComponent } from './components/video-block/video-block.component';
-import { TitleWithIconAndTextComponent } from './components/title-with-icon-and-text/title-with-icon-and-text.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { PreviewOnDifferentSizesComponent } from './components/preview-on-different-sizes/preview-on-different-sizes.component';
 
@@ -14,7 +13,6 @@ import { PreviewOnDifferentSizesComponent } from './components/preview-on-differ
   declarations: [
     CardWithIconComponent,
     VideoBlockComponent,
-    TitleWithIconAndTextComponent,
     MainPageComponent,
     PreviewOnDifferentSizesComponent,
   ],
