22fed46d69df9aaad41593ae2c0ffe05036b3633
Tanya Samal <solesytto@gmail.com>
2022-05-05 20:57:41 +0300

    feat: add auth guard

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { CanLoad, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import * as UserSelectors from '../../user/store/user.selectors';
+
+@Injectable()
+export class AuthGuard implements CanLoad {
+  private currentUser$ = this.store.select(UserSelectors.selectCurrentUser);
+
+  constructor(
+    private readonly router: Router,
+    private readonly store: Store,
+  ) {}
+
+  public canActivate(): boolean {
+    return this.canLoad();
+  }
+
+  public canLoad(): boolean {
+    let token = '';
+    this.currentUser$.subscribe((currUser) => {
+      token = (currUser) ? currUser.token : '';
+    });
+    if (token !== '') {
+      return true;
+    } else {
+      this.router.navigate(['/user', 'login'], {
+        queryParams: {
+          accessDenied: true,
+        },
+      });
+
+      return false;
+    }
+  }
+}
