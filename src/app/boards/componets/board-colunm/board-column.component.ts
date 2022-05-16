f76c13a66befc389b8fab05e3bb2cae07728d1ad
horndron <horndron@yandex.ru>
2022-05-16 11:02:31 +0300

    feat: add basic drag&drop

--- src/app/boards/componets/board-colunm/board-column.component.ts
+++ src/app/boards/componets/board-colunm/board-column.component.ts
@@ -1,6 +1,8 @@
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, Input } from '@angular/core';
 import { Column } from 'src/app/models/column';
+import { Task } from 'src/app/models/task';
+import { COLUMN } from './mock';
 
 @Component({
   selector: 'rsm-board-column',
@@ -9,8 +11,24 @@ import { Column } from 'src/app/models/column';
 })
 export class BoardColumnComponent {
   @Input() column: Column;
-
-  drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.column.tasks, event.previousIndex, event.currentIndex);
+  tasks = COLUMN.tasks as Task[];
+  drop(event: CdkDragDrop<Task[]>) {
+    console.log('tasks', event.container.data);
+    console.log('tasks', event.container.data);
+    console.log('event', event);
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 }
