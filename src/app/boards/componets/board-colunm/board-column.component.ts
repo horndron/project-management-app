380420cab7e7d31de92476d23cd45fdc51b00768
horndron <horndron@yandex.ru>
2022-05-18 13:50:27 +0300

    refactor: delete mock data

--- src/app/boards/componets/board-colunm/board-column.component.ts
+++ src/app/boards/componets/board-colunm/board-column.component.ts
@@ -1,8 +1,5 @@
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, Input } from '@angular/core';
 import { Column } from 'src/app/models/column';
-import { Task } from 'src/app/models/task';
-import { COLUMN } from './mock';
 
 @Component({
   selector: 'rsm-board-column',
